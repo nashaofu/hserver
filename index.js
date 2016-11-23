d66392fd1e31ad8fdb67e7d1ae772b30066c2f5e
程刁 <diaocheng@outlook.com>
2016-11-23 22:04:41 +0800

    完成基本框架
    modify context.js 编写错误处理，继承request与response
    modify request.js 完善accept及其accept相关参数
    modify response.js 完成response的各种数据获取与设置的方法
    modify README.MD 完成一个基本的file server例子

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 80;
 var app = new Server();
 
 // logger
-app.use(function*(next) {
+app.use(function* (next) {
     var start = new Date;
     yield next;
     var ms = new Date - start;
@@ -17,7 +17,7 @@ app.use(function*(next) {
 });
 
 // response
-app.use(function*(next) {
+app.use(function* (next) {
     let pathname = decodeURI(this.request.pathname);
     if (pathname.slice(-1) === '/') {
         pathname = path.join(pathname, 'index.html');
@@ -31,28 +31,29 @@ app.use(function*(next) {
         this.response.charset = 'utf-8';
     }
 });
-app.use(function*(next) {
+app.use(function* (next) {
     let path = this.request.pathname;
     let pathname = this.options['pathname'];
     let response = this.response;
     let res = this.res;
-    yield new Promise(function(resolve, reject) {
-        fs.stat(pathname, function(err, stats) {
+    let ctx = this;
+    yield new Promise(function (resolve, reject) {
+        fs.stat(pathname, function (err, stats) {
             if (err) {
-                response.status = 404;
-                reject(new Error(404));
+                ctx.status = 404;
+                reject(new Error(ctx.status));
             } else {
                 if (stats.isFile()) {
                     let type = mime.lookup(pathname);
                     let charset = mime.charsets.lookup(type);
-                    response.set('Content-Type', type + (charset ? '; charset=' + charset : ''));
+                    ctx.set('Content-Type', type + (charset ? '; charset=' + charset : ''));
                 } else if (stats.isDirectory()) {
-                    response.status = 301;
-                    response.set('Location', path + '/');
-                    reject(new Error(301));
+                    ctx.status = 301;
+                    ctx.set('Location', path + '/');
+                    reject(new Error(ctx.status));
                 } else {
-                    response.status = 400;
-                    reject(new Error(400));
+                    ctx.status = 400;
+                    reject(new Error(ctx.status));
                 }
             }
             resolve();
